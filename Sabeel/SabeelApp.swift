2285156f140fb683998b0c0c96fc05b2cb33de8a
Owais Quadri <owaisquadri01@gmail.com>
2023-08-05 23:35:37 -0400

    Initial Commit

---
+++ Sabeel/SabeelApp.swift
@@ -0,0 +1,20 @@
+//
+//  SabeelApp.swift
+//  Sabeel
+//
+//  Created by Owais on 2023-08-05.
+//
+
+import SwiftUI
+
+@main
+struct SabeelApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
